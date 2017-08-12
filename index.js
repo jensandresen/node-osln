2b85df7421d302013ae1f1b4b49840592c263b5b
Jens Andresen <jad@trifork.com>
2017-08-12 14:24:45 +0200

    ported powershell implementation to js - in promise style

---
+++ index.js
@@ -0,0 +1,46 @@
+const dir = require('node-dir');
+const readline = require('readline');
+const open = require("open");
+
+const sourceDir = __dirname;
+
+dir.promiseFiles(sourceDir)
+    .then(files => {
+        return files.filter(name => name.match(/\.txt$/))
+    })
+    .then(solutionFiles => {
+        if (solutionFiles.length === 0) {
+            console.log(`No solution file found in "${sourceDir}"`)
+            return;
+        }
+
+        console.log("");
+        console.log("Multiple solution files found...")
+        console.log("");
+
+        for(let i = 0; i < solutionFiles.length; i++) {
+            const option = solutionFiles[i];
+            console.log(`  ${i+1}: ${option}`);
+        }
+
+        console.log("");
+
+        const rl = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout
+        });
+
+        rl.question("Please select which to open: ", (answer) => {
+            const index = parseInt(answer) - 1;
+            const selected = solutionFiles[index];
+
+            if (selected) {
+                console.log(`Opening "${selected}"...`);
+                open(selected);
+            } else {
+                console.log(`Error, unrecognized value of "${answer}". The options was 1-${solutionFiles.length}.`);
+            }
+
+            rl.close();
+        });                
+    });
