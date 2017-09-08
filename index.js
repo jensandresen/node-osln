6868f3b826a5bda3428238caeba30d75ab08729b
Jens Andresen <jad@trifork.com>
2017-09-08 08:21:11 +0200

    add minimist as handler for command line argument parser

--- index.js
+++ index.js
@@ -3,7 +3,19 @@ const dir = require('node-dir');
 const readline = require('readline');
 const open = require("open");
 const leftPad = require("left-pad");
+const minimist = require("minimist");
 
+const commandProcessor = require("./commands");
+
+const args = process.argv.splice(2);
+
+const commandName = args[0];
+const commandArgs = minimist(args.slice(1));
+
+
+commandProcessor(commandName, commandArgs);
+
+/*
 const sourceDir = process.cwd();
 
 dir.promiseFiles(sourceDir)
@@ -53,3 +65,4 @@ dir.promiseFiles(sourceDir)
             rl.close();
         });                
     });
+    */
