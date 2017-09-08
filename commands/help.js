2a5ecf617fc79772e40a94fdbd28ca25b8ace34f
Jens Andresen <jad@trifork.com>
2017-09-08 20:53:43 +0200

    add extensive help information in a help command

--- commands/help.js
+++ commands/help.js
@@ -1,13 +1,53 @@
-const leftPad = require("left-pad");
 const rightPad = require("right-pad");
+const indendBaseline = "  ";
 
 module.exports = {
 
     execute: function(commandName, commandArgs, helpMessages) {
-        console.log(`Usage: osln <command> [<args>]`);
+        const commandsNeedingHelp = commandArgs["_"];
+
+        if (commandsNeedingHelp === undefined || commandsNeedingHelp === null || commandsNeedingHelp.length === 0) {
+            this.printAll(helpMessages);
+            return;
+        }
+
+        if (commandsNeedingHelp.length === 1) {
+            const name = commandsNeedingHelp[0];
+
+            const theCommand = helpMessages.find(x => x.name === name);
+            
+            if (theCommand) {
+                this.printSingle(theCommand);
+                return;
+            } else {
+                console.log(`Command "${name}" is unknown.`);
+                console.log();
+            }
+        } else {
+            console.log("Only specify one command name for detailed information and usage.")
+            console.log();
+        }
+
+        const helpMessageForHelpCommand = this.getHelp();        
+        this.printSingle(helpMessageForHelpCommand);
+    },
+    getHelp: function() {
+        return {
+            name: "help",
+            description: "Show information about all commands available or detailed usage for a specific command.",
+            usages: [
+                "help [command]",
+            ],
+            example: "osln help version",
+            footer: `For a list of available commands type "osln help".`
+        };
+    },
+    printAll: function(helpMessages) {
+        console.log(`Usage:`);
+        console.log(`${indendBaseline}osln <command> [<args>]`);
         console.log("");
+
         console.log("Available commands:");
-        console.log("");
         
         let maxLength = 0;
         helpMessages.forEach(function(element) {
@@ -18,10 +58,78 @@ module.exports = {
 
         for (var i = 0; i < helpMessages.length; i++) {
             var help = helpMessages[i];
-            console.log(`   ${rightPad(help.name, maxLength + 4)}${help.description}`);
+            console.log(`${indendBaseline}${rightPad(help.name, maxLength + 4)}${help.description}`);
         }
 
         console.log();
         console.log("Type \"osln help <command>\" for help information for a specific command.");
+    },
+    printSingle: function(command) {
+        const hasArguments = command.args && command.args.length > 0;
+
+        console.log(`Usage:`);
+        const usages = command.usages && command.usages.length > 0
+            ? command.usages
+            : [`${command.name}${hasArguments ? " [<args>]" : ""}`];
+
+        usages.forEach(usage => {
+            console.log(`${indendBaseline}osln ${usage}`);
+        });
+
+        console.log();
+        console.log("Description:")
+        console.log(`${indendBaseline}${command.description}`);
+
+        if (command.example) {
+            console.log();
+            console.log("Example:");
+            console.log(`${indendBaseline}${command.example}`);
+        }
+
+        if (hasArguments) {
+            console.log();
+            console.log("Available arguments:");
+            
+            const availableArgs = command.args.map(arg => {
+                let short = null;
+                if (arg.short) {
+                    short = `-${arg.short}`;
+                }
+
+                let long = null;
+                if (arg.long) {
+                    long = `--${arg.long}`;
+                }
+
+                if (short && long) {
+                    return {
+                        args: `${short}, ${long}`,
+                        description: arg.description
+                    };
+                }
+
+                return {
+                    args: [short, long].find(x => x != null),
+                    description: arg.description
+                };
+            });
+
+            let maxLength = 0;
+            availableArgs.forEach(arg => {
+                if (arg.args.length > maxLength) {
+                    maxLength = arg.args.length;
+                }
+            });
+
+            availableArgs.forEach(arg => {
+                const message = `${indendBaseline}${rightPad(arg.args, maxLength+4)}${arg.description}`;
+                console.log(message);
+            });
+        }
+
+        if (command.footer) {
+            console.log();
+            console.log(command.footer);
+        }
     }
 };
