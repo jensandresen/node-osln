f881f96a9f4057c82d7cb365cad35cc6bb415bbe
Thomas Wittrup Fischer <thofisch@gmail.com>
2019-06-27 18:49:28 +0200

    skip solutions from excluded paths for list and open commands

--- utils/settings.js
+++ utils/settings.js
@@ -8,6 +8,10 @@ module.exports = {
     getVersion: function() {
         const info = require("./../package.json");
         return info.version;
+    },
+
+    getExcludedPath: function() {
+        return [/node_modules/];
     }
 
 };
